f52c35feae3f2046578aa2479078425473739c64
yinrhh <1194301673@qq.com>
2023-03-20 19:25:39 +0800

    first commit

---
+++ spring-samples/src/main/java/org/springframework/Main.java
@@ -0,0 +1,11 @@
+package org.springframework;
+
+/**
+ * @author: YRH
+ * @date: ${DATE}
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
